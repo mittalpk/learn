3181fe0b6db09adfe162f53f8f8e75bbb3e21236
mittalpk <46550971+mittalpk@users.noreply.github.com>
2025-03-18 11:30:59 +0100

    Update DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -8,3 +8,46 @@ students = pd.melt(frame=students, id_vars=['full_name','gender_age','grade'], v
 print(students.head())
 print(students.columns)
 print(students.exam.value_counts())
+
+########################################
+
+duplicates = students.duplicated()
+print(duplicates.head())
+print(duplicates.value_counts())
+students = students.drop_duplicates()
+duplicates = students.duplicated()
+print(duplicates.head())
+print(duplicates.value_counts())
+
+
+##############Split Index##########################
+
+students["gender"] = students.gender_age.str[0:1]
+students["age"] = students.gender_age.str[1:3]
+print(students.head())
+
+students = students[['full_name','exam','grade','score','gender','age']]
+
+###################Split char ##################################
+
+name_split = students["full_name"].str.split(' ')
+
+students["first_name"] = name_split.str.get(0)
+students["last_name"] = name_split.str.get(1)
+
+print(students.head())
+
+###################String Parsing ##################################
+
+students["score"] = students["score"].replace('[\%,]','', regex=True)
+students["score"] = pd.to_numeric(students["score"])
+
+
+students.grade = students.grade.str.split('(\d+)', expand=True)[1]
+
+print(students.dtypes)
+
+students.grade = pd.to_numeric(students.grade)
+avg_grade = students.grade.mean()
+
+print(avg_grade)
